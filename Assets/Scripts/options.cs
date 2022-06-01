44ebb20b92e2c6a0818ac3b49e97efd80b35eba3
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2022-06-01 21:47:02 +0200

    Add menu + WIP options

---
+++ Assets/Scripts/options.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class options : MonoBehaviour
+{
+    public GameObject Panel;
+    public Dropdown DResolution;
+    public AudioSource audioSource;
+    public Slider SliderVolume;
+    public Text TextVolume;
+    bool visible = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            visible = !visible;
+            Panel.SetActive(visible);
+        }
+    }
+    
+    public void SetResolution()
+    {
+        switch (DResolution.value)
+        {
+            case 0:
+                Screen.SetResolution(640, 360, true);
+                break;
+            
+            case 1:
+                Screen.SetResolution(720, 576, true);
+                break;
+            
+            case 2:
+                Screen.SetResolution(1280, 720, true);
+                break;
+            
+            case 3:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            
+            case 4:
+                Screen.SetResolution(3840, 2160, true);
+                break;
+            
+            case 5:
+                Screen.SetResolution(7680, 4320, true);
+                break;
+        }
+    }
+
+    public void SliderChanger()
+    {
+        audioSource.volume = SliderVolume.value;
+        TextVolume.text = "Volume " + (audioSource.volume * 100).ToString("00") + "%";
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToGame()
+    {
+        visible = !visible;
+        Panel.SetActive(visible);
+    }
+}
